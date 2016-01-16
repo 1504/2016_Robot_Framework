bd584e5889a413590c596ffe9d40566450528cd5
Eashwar Mohan <luigi629@gmail.com>
2016-01-16 10:03:54 -0500

    fixed IO, updated front_side
    TODO: endgame, arcade, shooter/pickup.

--- src/org/usfirst/frc/team1504/robot/Drive.java
+++ src/org/usfirst/frc/team1504/robot/Drive.java
@@ -7,89 +7,92 @@ import org.usfirst.frc.team1504.robot.Update_Semaphore.Updatable;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DriverStation;
 
-public class Drive implements Updatable {
-	
+public class Drive implements Updatable
+{
+
 	private static class DriveTask implements Runnable
 	{
 
-        private Drive _d;
+		private Drive _d;
 
-        DriveTask(Drive d)
-        {
-            _d = d;
-        }
+		DriveTask(Drive d)
+		{
+			_d = d;
+		}
 
-        public void run()
-        {
-            _d.fastTask();
-        }
-    }
-	
+		public void run()
+		{
+			_d.fastTask();
+		}
+	}
+
 	private static final Drive instance = new Drive();
-	
+
 	private Thread _task_thread;
 	private Thread _dump_thread;
 	private volatile boolean _thread_alive = true;
-	
-    /**
-     * Gets an instance of the Drive
-     *
-     * @return The Drive.
-     */
-    public static Drive getInstance()
-    {
-        return Drive.instance;
-    }
-    
+
+	/**
+	 * Gets an instance of the Drive
+	 *
+	 * @return The Drive.
+	 */
+	public static Drive getInstance()
+	{
+		return Drive.instance;
+	}
+
 	protected Drive()
 	{
 		_task_thread = new Thread(new DriveTask(this), "1504_Drive");
 		_task_thread.setPriority((Thread.NORM_PRIORITY + Thread.MAX_PRIORITY) / 2);
 		_task_thread.start();
-		
+
 		Update_Semaphore.getInstance().register(this);
-		
+
 		DriveInit();
-		
+
 		System.out.println("Drive Initialized");
 	}
-	
+
 	public void release()
 	{
 		_thread_alive = false;
-    }
-	
-/** 
- * 
- * User code here
- * 
- */
-	
+	}
+
+	/**
+	 * 
+	 * User code here
+	 * 
+	 */
+
 	private DriverStation _ds = DriverStation.getInstance();
 	private Logger _logger = Logger.getInstance();
 	private volatile boolean _new_data = false;
-	private volatile double[] _input = {0.0, 0.0};// TWO due to ARCADE DRIVE.
+	private volatile double[] _input =
+	{ 0.0, 0.0 };// TWO due to ARCADE DRIVE.
 	private DriveGlide _glide = new DriveGlide();
 	private Groundtruth _groundtruth = Groundtruth.getInstance();
-	
+
 	private CANTalon[] _motors = new CANTalon[Map.DRIVE_MOTOR_PORTS.length];
-	
+
 	private volatile int _loops_since_last_dump = 0;
-	
+
 	/**
 	 * Set up everything that will be needed for the drive class
 	 */
 	private void DriveInit()
 	{
 		// Set up the drive motors
-		for(int i = 0; i < Map.DRIVE_MOTOR_PORTS.length; i++)
+		for (int i = 0; i < Map.DRIVE_MOTOR_PORTS.length; i++)
 		{
 			_motors[i] = new CANTalon(Map.DRIVE_MOTOR_PORTS[i]);
 		}
 	}
-	
+
 	/**
 	 * Method called when there is new data from the Driver Station.
+	 * 
 	 * @see org.usfirst.frc.team1504.robot.Update_Semaphore
 	 */
 	public void semaphore_update()
@@ -99,37 +102,42 @@ public class Drive implements Updatable {
 		drive_inputs(IO.tank_input());
 		// so "_new_data = true" at the VERY END OF EVERYTHING
 	}
-	
+
 	/**
-	 * Put data into the processing queue.
-	 * Usable from both the semaphore and autonomous methods.
+	 * Put data into the processing queue. Usable from both the semaphore and
+	 * autonomous methods.
 	 */
 	public void drive_inputs(double forward, double turn)
 	{
-		double[] inputs = {forward, turn};
+		double[] inputs =
+		{ forward, turn };
 		drive_inputs(inputs);
 	}
+
 	public void drive_inputs(double[] input)
 	{
-		if(_new_data)
+		if (_new_data)
 			return;
-		
+
 		_input = input;
 		_new_data = true;
 	}
-	
+
 	/**
-	 * Programmatically switch the direction the robot goes when the stick gets pushed; due to tank, can only switch between forward and backwards.
+	 * Programmatically switch the direction the robot goes when the stick gets
+	 * pushed; due to tank, can only switch between forward and backwards.
 	 */
-	private double[] front_side(double[] input) {
-		input[0] = input[0]*-1;
+	private double[] front_side(double[] input)
+	{
+		input[0] = input[0] * -1;
 		return input;
 	}
-	
+
 	/**
 	 * Detented controller correction methods (and helper methods)
 	 */
-	private double[] detents(double[] input) {
+	private double[] detents(double[] input)
+	{
 
 		double theta = Math.atan2(input[0], input[1]);
 
@@ -144,19 +152,27 @@ public class Drive implements Updatable {
 
 		return detented;
 	}
-	private double correct_x(double theta) {
+
+	private double correct_x(double theta)
+	{
 		return -Math.sin(theta) * (-Math.sin(8 * theta) - 0.25 * Math.sin(4 * theta));
 	}
-	private double correct_y(double theta) {
+
+	private double correct_y(double theta)
+	{
 		return Math.cos(theta) * (-Math.sin(8 * theta) - 0.25 * Math.sin(4 * theta));
 	}
-	private double distance(double x, double y) {
+
+	private double distance(double x, double y)
+	{
 		return Math.sqrt(x * x + y * y);
 	}
-	
+
 	/**
 	 * Ground truth sensor corrections
-	 * @param input - Joystick input to correct towards
+	 * 
+	 * @param input
+	 *            - Joystick input to correct towards
 	 * @return
 	 */
 	private double[] groundtruth_correction(double[] input)
@@ -164,111 +180,124 @@ public class Drive implements Updatable {
 		double[] normal_input = input;
 		double[] output = input;
 		double[] speeds = _groundtruth.getSpeed();
-		
+
 		// Normalize the inputs and actual speeds
-		if(groundtruth_normalize(speeds) == 0)
+		if (groundtruth_normalize(speeds) == 0)
 			return input;
 		groundtruth_normalize(normal_input);
-		
+
 		// Apply P(ID) correction factor to the joystick values
 		// TODO: Determine gain constant and add to the Map
-		for(int i = 0; i < 3; i++)
+		for (int i = 0; i < 3; i++)
 			output[i] += (normal_input[i] - speeds[i]) * -0.01;
-		
+
 		return output;
 	}
-	
+
 	/**
 	 * Normalization function for arrays to normalize full scale to +- 1 <br>
-	 * Note: THIS FUNCTION OPERATES ON THE REFERENCE INPUT ARRAY AND WILL CHANGE IT!
-	 * @param input - The array to normalize
+	 * Note: THIS FUNCTION OPERATES ON THE REFERENCE INPUT ARRAY AND WILL CHANGE
+	 * IT!
+	 * 
+	 * @param input
+	 *            - The array to normalize
 	 * @return Maximum value in the array
 	 */
 	private double groundtruth_normalize(double[] input)
 	{
 		double max = 0;
-		for(int i = 0; i < input.length; i++)
+		for (int i = 0; i < input.length; i++)
 			max = Math.max(Math.abs(input[1]), max);
-		
-		if(max == 0)
+
+		if (max == 0)
 			return 0;
-		
+
 		max = max == 0 ? 1 : max;
-		for(int i = 0; i < input.length; i++)
+		for (int i = 0; i < input.length; i++)
 			input[i] /= max;
-		
+
 		return max;
 	}
-	
+
 	/**
 	 * Convert the Forward/Backward and Turn values into 4 motor outputs
-	 * @param input - Double array containing Forward and Turn values
-	 * @param output - Double array containing motor output values
+	 * 
+	 * @param input
+	 *            - Double array containing Forward and Turn values
+	 * @param output
+	 *            - Double array containing motor output values
 	 */
-	private double[] outputCompute(double[] input) {
+	private double[] outputCompute(double[] input)
+	{
 		double[] output = new double[4];
 		double[] scaled_input = new double[2];
-		
+
 		double theta = Math.atan2(input[0], input[1]);
-		
-		if (theta > (Math.PI/4)) {theta = (Math.PI/2) - theta;}
-		
-		double alpha = theta + (Math.PI/4);
+
+		if (theta > (Math.PI / 4))
+		{
+			theta = (Math.PI / 2) - theta;
+		}
+
+		double alpha = theta + (Math.PI / 4);
 		double hyp = Math.sqrt((input[0] * input[0]) + (input[1] * input[1]));
-		
-		double hyp_new = Math.cos(theta)*hyp/Math.cos(alpha);
-		scaled_input[0] = Math.sin(alpha)*hyp_new;
-		scaled_input[1] = Math.cos(alpha)*hyp_new;
-		
+
+		double hyp_new = Math.cos(theta) * hyp / Math.cos(alpha);
+		scaled_input[0] = Math.sin(alpha) * hyp_new;
+		scaled_input[1] = Math.cos(alpha) * hyp_new;
+
 		output[0] = (scaled_input[1] - scaled_input[0]);
 		output[1] = (scaled_input[1] - scaled_input[0]);
 		output[2] = (scaled_input[1] + scaled_input[0]);
 		output[3] = (scaled_input[1] + scaled_input[0]);
-		
-		
+
 		return output;
 	}
-	
+
 	/**
-	 * Output values to motors.
-	 * Input: array of motor values to output in Map.DRIVE_MOTOR order.
+	 * Output values to motors. Input: array of motor values to output in
+	 * Map.DRIVE_MOTOR order.
 	 */
-	private void motorOutput(double[] values) {
-		for(int i = 0; i < _motors.length; i++)
+	private void motorOutput(double[] values)
+	{
+		for (int i = 0; i < _motors.length; i++)
 		{
 			// There are no Sync Groups for CANTalons. Apparently.
 			_motors[i].set(values[i] * Map.DRIVE_OUTPUT_MAGIC_NUMBERS[i]);
 		}
 	}
-	
+
 	/**
 	 * Dump class for logging
 	 */
 	private void dump()
 	{
-		byte[] output = new byte[12+4+4];
-		
+		byte[] output = new byte[12 + 4 + 4];
+
 		int loops_since_last_dump = _loops_since_last_dump;
-		//_loops_since_last_dump = 0;
-		
+		// _loops_since_last_dump = 0;
+
 		// Dump motor set point, current, and voltage
-		for(int i = 0; i < Map.DRIVE_MOTOR.values().length; i++)
+		for (int i = 0; i < Map.DRIVE_MOTOR.values().length; i++)
 		{
-			output[i] = Utils.double_to_byte(_motors[i].get()); // Returns as 11-bit, downconvert to 8
-			output[i+1] = (byte) _motors[i].getOutputCurrent();
-			output[i+2] = (byte) (_motors[i].getBusVoltage() * 10);
+			output[i] = Utils.double_to_byte(_motors[i].get()); // Returns as
+																// 11-bit,
+																// downconvert
+																// to 8
+			output[i + 1] = (byte) _motors[i].getOutputCurrent();
+			output[i + 2] = (byte) (_motors[i].getBusVoltage() * 10);
 			// From CANTalon class: Bus voltage * throttle = output voltage
 		}
 		ByteBuffer.wrap(output, 12, 4).putInt(loops_since_last_dump);
-		ByteBuffer.wrap(output, 16, 4).putInt((int)(System.currentTimeMillis() - IO.ROBOT_START_TIME));
-		
-		if(_logger != null)
+		ByteBuffer.wrap(output, 16, 4).putInt((int) (System.currentTimeMillis() - IO.ROBOT_START_TIME));
+
+		if (_logger != null)
 		{
-			if(_logger.log(Map.LOGGED_CLASSES.DRIVE, output))
+			if (_logger.log(Map.LOGGED_CLASSES.DRIVE, output))
 				_loops_since_last_dump -= loops_since_last_dump;
 		}
 	}
-	
+
 	/**
 	 * Update motors as fast as possible, but only compute all the joystick stuff when there's new data
 	 */
@@ -290,14 +319,14 @@ public class Drive implements Updatable {
 					if(_ds.isOperatorControl())
 					{
 						// Switch front side if we need to
-						double rotation_offset = IO.front_side();
-						if(!Double.isNaN(rotation_offset)){}
-							//setFrontAngle(rotation_offset);
-					
+						boolean is_frontside = IO.front_side();
 						// Detents
 						input = detents(input);
 						// Frontside
-						input = front_side(input);
+						if (is_frontside)
+						{
+							input = front_side(input);
+						}
 						// Orbit point
 						//input = orbit_point(input);
 						// Glide
--- src/org/usfirst/frc/team1504/robot/IO.java
+++ src/org/usfirst/frc/team1504/robot/IO.java
@@ -2,8 +2,7 @@ package org.usfirst.frc.team1504.robot;
 
 public class IO
 {
-//	private static Latch_Joystick _drive_forwardright = new Latch_Joystick(Map.DRIVE_FORWARDRIGHT_JOYSTICK);
-//	private static Latch_Joystick _drive_rotation = new Latch_Joystick(Map.DRIVE_ROTATION_JOYSTICK);
+
 	
 	private static Latch_Joystick _drive_y = new Latch_Joystick(Map.DRIVE_ARCADE_Y);
 	private static Latch_Joystick _drive_w = new Latch_Joystick(Map.DRIVE_ARCADE_TURN);
@@ -26,37 +25,31 @@ public class IO
 		
 		
 		inputs[0] = Map.DRIVE_INPUT_MAGIC_NUMBERS[0] * Math.pow(Utils.deadzone(_drive_y.getRawAxis(Map.JOYSTICK_Y_AXIS)), 2) * Math.signum(_drive_y.getRawAxis(Map.JOYSTICK_Y_AXIS));//forward/backward motion
-		inputs[1] = Map.DRIVE_INPUT_MAGIC_NUMBERS[1] * Math.pow(Utils.deadzone(_drive_w.getRawAxis(Map.JOYSTICK_X_AXIS)), 2) * Math.signum(_drive_w.getRawAxis(Map.JOYSTICK_x_AXIS)) * -1;//turning left/right;
+		inputs[1] = Map.DRIVE_INPUT_MAGIC_NUMBERS[1] * Math.pow(Utils.deadzone(_drive_w.getRawAxis(Map.JOYSTICK_X_AXIS)), 2) * Math.signum(_drive_w.getRawAxis(Map.JOYSTICK_X_AXIS)) * -1;//turning left/right;
 		
 		return inputs;
 	}
 	
 	
-	public static double[] mecanum_input() {
-		double[] inputs = new double[3];
-
-		inputs[0] = Map.DRIVE_INPUT_MAGIC_NUMBERS[0] * Math.pow(Utils.deadzone(_drive_forwardright.getRawAxis(Map.JOYSTICK_Y_AXIS)), 2) * Math.signum(_drive_forwardright.getRawAxis(Map.JOYSTICK_Y_AXIS));// y
-		inputs[1] = Map.DRIVE_INPUT_MAGIC_NUMBERS[1] * Math.pow(Utils.deadzone(_drive_forwardright.getRawAxis(Map.JOYSTICK_X_AXIS)), 2) * Math.signum(_drive_forwardright.getRawAxis(Map.JOYSTICK_X_AXIS));// x
-		inputs[2] = Map.DRIVE_INPUT_MAGIC_NUMBERS[2] * Math.pow(Utils.deadzone(_drive_rotation.getRawAxis(Map.JOYSTICK_X_AXIS)), 2) * Math.signum(_drive_rotation.getRawAxis(Map.JOYSTICK_X_AXIS));// w
-		
-		//inputs[0] = _drive_forwardright.getRawAxis(Map.JOYSTICK_Y_AXIS);
-		//inputs[1] = _drive_forwardright.getRawAxis(Map.JOYSTICK_X_AXIS);
-		//inputs[2] = _drive_rotation.getRawAxis(Map.JOYSTICK_X_AXIS);
-		
-		return inputs;
-	}
+//	public static double[] mecanum_input() {
+//		double[] inputs = new double[3];
+//
+//		inputs[0] = Map.DRIVE_INPUT_MAGIC_NUMBERS[0] * Math.pow(Utils.deadzone(_drive_forwardright.getRawAxis(Map.JOYSTICK_Y_AXIS)), 2) * Math.signum(_drive_forwardright.getRawAxis(Map.JOYSTICK_Y_AXIS));// y
+//		inputs[1] = Map.DRIVE_INPUT_MAGIC_NUMBERS[1] * Math.pow(Utils.deadzone(_drive_forwardright.getRawAxis(Map.JOYSTICK_X_AXIS)), 2) * Math.signum(_drive_forwardright.getRawAxis(Map.JOYSTICK_X_AXIS));// x
+//		inputs[2] = Map.DRIVE_INPUT_MAGIC_NUMBERS[2] * Math.pow(Utils.deadzone(_drive_rotation.getRawAxis(Map.JOYSTICK_X_AXIS)), 2) * Math.signum(_drive_rotation.getRawAxis(Map.JOYSTICK_X_AXIS));// w
+//		
+//		//inputs[0] = _drive_forwardright.getRawAxis(Map.JOYSTICK_Y_AXIS);
+//		//inputs[1] = _drive_forwardright.getRawAxis(Map.JOYSTICK_X_AXIS);
+//		//inputs[2] = _drive_rotation.getRawAxis(Map.JOYSTICK_X_AXIS);
+//		
+//		return inputs;
+//	}
 	
 	
-	public static double front_side() {
-		if (_drive_rotation.getRawButtonLatch(Map.DRIVE_FRONTSIDE_BACK)) {
-			return 180.0;
-		} else if (_drive_rotation.getRawButtonLatch(Map.DRIVE_FRONTSIDE_RIGHT)) {
-			return 270.0;
-		} else if (_drive_rotation.getRawButtonLatch(Map.DRIVE_FRONTSIDE_FRONT)) {
-			return 0.0;
-		} else if (_drive_rotation.getRawButtonLatch(Map.DRIVE_FRONTSIDE_LEFT)) {
-			return 90.0;
-		}
-		return Double.NaN;
+	public static boolean front_side() {
+		if (_drive_w.getRawButtonLatch(Map.DRIVE_FRONTSIDE_BACK)) {
+			return true;
+		} 
+		return false;
 	}
 }
